1649df0be60b0b239a8a827298843a7f64cbe7bc
Shixian Li <lsx7@sina.com>
2023-04-21 07:47:52 +0800

    Update: Use 2023-API to download website and save raw data html files

--- download_tutorials.py
+++ download_tutorials.py
@@ -1,68 +1,68 @@
 import io
+import os
 import re
+import glob
 import gzip
 import urllib.request
 
 from lxml import etree
 
+HOME_PAGE = 'http://www.runoob.com'
+errors = []
 
-def getcontent(url):
-    if not url.startswith('http:'):
-        url = 'http:' + url
 
-    mainpage = urllib.request.urlopen(url)
-    type = mainpage.info().get('Content-Encoding')
-    if type == 'gzip':
-        mainpage = urllib.request.urlopen(url)
-        tmp = io.StringIO.StringIO(mainpage.read())
-        data = gzip.GzipFile(fileobj=tmp)
+def SaveHtml(url, data):
+    path = url.replace(HOME_PAGE, 'html')
+    folder = os.path.dirname(path)
+    if folder:
+        os.makedirs(folder, exist_ok=True)
+    with open(path, 'w', encoding='u8') as f:
+        f.write(data)
+
+
+def GetContent(url):
+    path = url.replace(HOME_PAGE, 'html')
+    if os.path.exists(path) and os.path.getsize(path): # read from cache
+        with open(path, 'rb') as f:
+            return f.read()
     else:
-        data = mainpage
-    return data
+        req = urllib.request.urlopen(url)
+        if 'gzip' == req.info().get('Content-Encoding'):
+            f = io.StringIO.StringIO(req.read())
+            req = gzip.GzipFile(fileobj=f)
+        return req.read()
 
 
-def gettutorial(title, url):
-    print('[**]' + url)
-    num = 0
-    file = open(title + '.html', 'a')
-    content = getcontent(url)
-    html = etree.HTML(content.read())
-    url = html.xpath('/html/body/div[3]/div/div[1]/div[2]/div//a/@href')
-    pre = ''
-    for item in url:
-        mytest = item.split('/')
-        if len(mytest) == 3:
-            pre = mytest[1]
-        elif len(mytest) == 1:
-            item = '/' + pre + '/' + item
-        else:
-            continue
-        item = 'http://www.runoob.com' + item
-        print('[**]\t' + item)
-        data = getcontent(item)
-
-        # replace js/css/images
-        if num == 0:
-            for tmp in data.readlines()[:-1]:
-                tmp = tmp.replace('/wp-content/themes/runoob/', './runoob/').replace('//cdn.bootcss.com/font-awesome/4.7.0/', './runoob/').replace('//cdn.bootcss.com/jquery/2.0.3', './runoob/')
-                file.write(tmp)
-        else:
-            for tmp in data.readlines()[24:-1]:
-                tmp = tmp.replace('/wp-content/themes/runoob/', './runoob/').replace('//cdn.bootcss.com/font-awesome/4.7.0/', './runoob/').replace('//cdn.bootcss.com/jquery/2.0.3', './runoob/')
-                file.write(tmp)
-        num += 1
-    file.write('</html>')
-    file.close()
+def GetSubTutorial(title, url):
+    print('- ' + url)
+    data = GetContent(url)
+    html = etree.HTML(data)
+    urls = html.xpath('/html/body/div[4]/div/div/div/div[2]/div/a/@href')
+    base = os.path.dirname(url) + '/'
+    for url2 in urls:
+        url2 = HOME_PAGE + url2 if '/' in url2 else base + url2
+        print('| - ' + url2)
+        try:
+            data2 = GetContent(url2).decode()
+            SaveHtml(url2, data2)
+        except Exception:
+            errors.append(url2)
 
 
-data = getcontent('http://www.runoob.com')
-html = etree.HTML(data.read())
-theme = html.xpath('/html/body/div[4]/div/div[2]//a')
-num = 0
-for a in theme:
-    title = theme[num].xpath('./h4/text()')[0].strip()
-    title = re.sub('/', '-', title)
-    suburl = theme[num].xpath('./@href')[0].strip()
-    suburl = str(suburl)
-    gettutorial(title, suburl)
-    num += 1
+def GetTutorial():
+    url = HOME_PAGE + '/index.html'
+    data = GetContent(url)
+    SaveHtml(url, data.decode())
+    html = etree.HTML(data)
+    theme = html.xpath('/html/body/div[4]/div/div[2]/div/a')
+    for a in theme:
+        title = a.xpath('./h4/text()')[0].strip()
+        title = re.sub('/', '-', title)
+        url = 'http:' + a.xpath('./@href')[0].strip()
+        GetSubTutorial(title, url)
+
+
+if __name__ == '__main__':
+    GetTutorial()
+    print('errors:')
+    print('\n'.join(errors))
